4d9a37c6f06c7f4b74cdd78bb9deb1ae8baad07e
rishabhgarg <rishabhgargdps@gmail.com>
2020-08-10 11:54:44 +0530

    Login page functionality added

---
+++ client/src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+<v-app>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Login</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            v-model="email"
+            label="Email"/>
+          <v-text-field
+          v-model="password"
+          label="Password"
+          type="password"/>
+          <div class = "error" v-html="error"/>
+          <v-btn class="cyan"
+            dark
+            @click="login">Login
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+</v-app>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  color: red;
+}
+</style>
